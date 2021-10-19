88ef013afdf59282c956683d224cc7be7c574779
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-10-19 08:19:17 +0200

    Add project files.

---
+++ BMI_IT1A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BMI_IT1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
